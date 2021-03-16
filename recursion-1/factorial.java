21c4b60d41067ffeca42a708385eb21497499708
태양 <56237265+taeyangcode@users.noreply.github.com>
2021-03-16 06:23:01 -0700

    Update factorial.java

--- recursion-1/factorial.java
+++ recursion-1/factorial.java
@@ -2,6 +2,7 @@
 Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
 Compute the result recursively (without loops).
 
+
 factorial(1) → 1
 factorial(2) → 2
 factorial(3) → 6
