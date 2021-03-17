a870aa1712c644d5d3709af9ef6523932f03c5be
태양 <56237265+taeyangcode@users.noreply.github.com>
2021-03-17 00:45:28 -0700

    Create count11.java

---
+++ recursion-1/count11.java
@@ -0,0 +1,13 @@
+/*
+Given a string, compute recursively (no loops) the number of "11" substrings in the string. The "11" substrings should not overlap.
+
+
+count11("11abc11") → 2
+count11("abc11x11x11") → 3
+count11("111") → 1
+*/
+
+public int count11(String str) {
+  if(str.length() < 2) return 0;
+  return str.substring(0, 2).equals("11") ? 1 + count11(str.substring(2)) : count11(str.substring(1));
+}
