edc0c625f7d92a9c185c3e34bb88d5501831e97f
태양 <56237265+taeyangcode@users.noreply.github.com>
2021-03-17 02:05:26 -0700

    Create nestParen.java

---
+++ recursion-1/nestParen.java
@@ -0,0 +1,14 @@
+/*
+Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))". 
+Suggestion: check the first and last chars, and then recur on what's inside them.
+
+
+nestParen("(())") → true
+nestParen("((()))") → true
+nestParen("(((x))") → false
+*/
+
+public boolean nestParen(String str) {
+  if(str.isEmpty()) return true;
+  return str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')' ? nestParen(str.substring(1, str.length() - 1)) : false;
+}
